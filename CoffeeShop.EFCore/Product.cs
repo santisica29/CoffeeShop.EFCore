2d4179417511438b91250087499f4fbefa4dde8f
Santiago Sica <santisica29@gmail.com>
2026-02-22 12:48:31 -0300

    Add DBContext Class and started migration process

---
+++ CoffeeShop.EFCore/Product.cs
@@ -0,0 +1,6 @@
+﻿namespace CoffeeShop.EFCore;
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ CoffeeShop.EFCore/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.EFCore;
+internal class ProductContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB; Initial Catalog=CoffeeShop; Integrated Security=True;");
+    }
+
+}
