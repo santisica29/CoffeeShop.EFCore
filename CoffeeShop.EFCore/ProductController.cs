5932ea01c68eded278a95f5bc00b4cac440ed797
Santiago Sica <santisica29@gmail.com>
2026-02-22 12:39:38 -0300

    Add project files.

---
+++ CoffeeShop.EFCore/ProductController.cs
@@ -0,0 +1,29 @@
+﻿namespace CoffeeShop.EFCore;
+
+internal class ProductController
+{
+    internal static void AddProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void DeleteProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UpdateProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void ViewAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void ViewProduct()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CoffeeShop.EFCore/Program.cs
@@ -0,0 +1,44 @@
+﻿using CoffeeShop.EFCore;
+using Spectre.Console;
+
+var isAppRunning = true;
+
+while (isAppRunning)
+{
+    var option = AnsiConsole.Prompt(
+    new SelectionPrompt<MenuOptions>()
+    .Title("What would you like to do?")
+    .AddChoices(Enum.GetValues<MenuOptions>()));
+
+    switch (option)
+    {
+        case MenuOptions.AddProduct:
+            ProductController.AddProduct();
+            break;
+        case MenuOptions.DeleteProduct:
+            ProductController.DeleteProduct();
+            break;
+        case MenuOptions.UpdateProduct:
+            ProductController.UpdateProduct();
+            break;
+        case MenuOptions.ViewProduct:
+            ProductController.ViewProduct();
+            break;
+        case MenuOptions.ViewAllProducts:
+            ProductController.ViewAllProducts();
+            break;
+        case MenuOptions.Quit:
+            isAppRunning = false;
+            break;
+    }
+}
+
+enum MenuOptions
+{
+    AddProduct,
+    DeleteProduct,
+    UpdateProduct,
+    ViewProduct,
+    ViewAllProducts,
+    Quit
+}
